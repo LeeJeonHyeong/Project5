924243f62c1a6947c58fb22d43b4ee571cd8d7ab
Lee Jeon Hyeong <jhlove0490@gmail.com>
2019-03-19 20:42:32 +0900

    Struct Example

---
+++ Project5/소스.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+#define MAX 10
+
+int main()
+{
+	struct MEMINFO
+	{
+		char name[20];
+		int salary;
+	};
+
+	MEMINFO person[MAX] = {
+	{ "김대표", 10000 },
+	{ "이부장", 7500},
+	{"홍과장", 6000},
+	};
+
+	MEMINFO* ptr;
+
+	ptr = person;
+	int count = 3;
+
+	for (; count < MAX; count++) {
+		cout << "이름은?";
+		cin >> (ptr + count)->name;
+		if (strcmp((ptr + count)->name, "end") == 0)
+			break;
+		cout << "연봉은?";
+		cin >> (ptr + count)->salary;
+	}
+
+	for (count--; count >= 0; count--)
+		cout << (ptr + count)->name << "의 연봉은"
+		<< (ptr + count)->salary << "입니다." << endl;
+	return 0;
+}
